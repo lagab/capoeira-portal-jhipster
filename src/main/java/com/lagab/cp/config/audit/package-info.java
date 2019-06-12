cac1c776c2a5303e8b1963045f304ec976465247
GABRIEL Ladislas <ladislas.gabriel.ext@humanis.com>
2019-06-12 15:14:41 +0200

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/lagab/cp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.lagab.cp.config.audit;
---
+++ src/main/java/com/lagab/cp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.lagab.cp.repository;
+
+import com.lagab.cp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
